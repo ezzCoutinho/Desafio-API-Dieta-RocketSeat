ec7f310fa3f47a36d2b9097736ef7438594eac7b
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-05 17:37:58 -0300

    Adding models in diet

---
+++ models/diets.py
@@ -0,0 +1,10 @@
+from database import db
+from sqlalchemy import func
+
+class Diets(db.Model):
+  # name (text), description (text), date(DateTime), is not diet(boolean)
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(80), nullable=False)
+  description = db.Column(db.String(80), nullable=False)
+  date = db.Column(db.DateTime, default=func.now())
+  is_not_diet = db.Column(db.Boolean, default=False)
